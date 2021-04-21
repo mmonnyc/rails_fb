9074c67f0c6208d4f5f0b15051b34da37b6875b9
mmonnyc <monika.vaitu@gmail.com>
2021-04-21 16:20:09 +0300

    add posts model and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validate :picture_size
-
+  has_many :posts
+  
   private 
 
   def picture_size
