35a90afd7bd2491c74099ef1be23085e3887be79
mmonnyc <monika.vaitu@gmail.com>
2021-04-21 17:12:31 +0300

    add notifications model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def new_notification(user, notice_id, notice_type)
+    notice = user.notifications.build(notice_id: notice_id, notice_type: notice_type)
+    user.notice_seen = false
+    user.save
+    notice
+  end
+
+  def notification_find(notice, type)
+    return User.find(notice.notice_id) if type == 'friendRequest'
+    return Post.find(notice.notice_id) if type == 'comment'
+    return Post.find(notice.notice_id) if type == 'like-post'
+    return unless type == 'like-comment'
+    comment = Comment.find(notice.notice_id)
+    Post.find(comment.post_id)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,21 +8,22 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes, dependent: :destroy
+  has_many :notifications, dependent: :destroy
 
-  has_many :sent_requests, class_name: 'Friendship',
+  has_many :friend_sent, class_name: 'Friendship',
                           foreign_key: 'sent_by_id',
                           inverse_of: 'sent_by',
                           dependent: :destroy
-  has_many :received_requests, class_name: 'Friendship',
+  has_many :friend_request, class_name: 'Friendship',
                           foreign_key: 'sent_to_id',
                           inverse_of: 'sent_to',
                           dependent: :destroy
   has_many :friends, -> { merge(Friendship.friends) },
-            through: :sent_requests, source: :sent_to
+            through: :friend_sent, source: :sent_to
   has_many :pending_requests, -> { merge(Friendship.not_friends) },
-            through: :sent_requests, source: :sent_to
+            through: :friend_sent, source: :sent_to
   has_many :received_requests, -> { merge(Friendship.not_friends) },
-            through: :received_requests, source: :sent_by
+            through: :friend_request, source: :sent_by
     
   private 
 
