96522f4ad44110220b3e4e0739078532eb776fae
mmonnyc <monika.vaitu@gmail.com>
2021-04-21 17:00:11 +0300

    add friendship controller and update models

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,21 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes, dependent: :destroy
 
+  has_many :sent_requests, class_name: 'Friendship',
+                          foreign_key: 'sent_by_id',
+                          inverse_of: 'sent_by',
+                          dependent: :destroy
+  has_many :received_requests, class_name: 'Friendship',
+                          foreign_key: 'sent_to_id',
+                          inverse_of: 'sent_to',
+                          dependent: :destroy
+  has_many :friends, -> { merge(Friendship.friends) },
+            through: :sent_requests, source: :sent_to
+  has_many :pending_requests, -> { merge(Friendship.not_friends) },
+            through: :sent_requests, source: :sent_to
+  has_many :received_requests, -> { merge(Friendship.not_friends) },
+            through: :received_requests, source: :sent_by
+    
   private 
 
   def picture_size
